74c74159ea61fe72ee0275a3ad8b1591efcf4ad4
Sulakshana Geeth <sulakshanageeth@gmail.com>
2022-08-12 08:53:42 +0530

    revert to the previous commit

--- Client/lib/components/constant/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.BACKEND_BASE_URL = void 0;
-var BACKEND_BASE_URL = "https://vehicle-registration.herokuapp.com";
-exports.BACKEND_BASE_URL = BACKEND_BASE_URL;
